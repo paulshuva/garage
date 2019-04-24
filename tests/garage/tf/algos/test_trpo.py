c676ed89b2d751e21d5c4d6bebfb4e28a4e2062d
CatherineSue <changsu.cs.work@gmail.com>
2019-04-24 15:32:28 -0700

    Add more unittests

--- tests/garage/tf/algos/test_trpo.py
+++ tests/garage/tf/algos/test_trpo.py
@@ -18,7 +18,7 @@ class TestTRPO(TfGraphTestCase):
     def test_trpo_pendulum(self):
         """Test TRPO with Pendulum environment."""
         with LocalRunner(self.sess) as runner:
-            env = TfEnv(normalize(gym.make("InvertedDoublePendulum-v2")))
+            env = TfEnv(normalize(gym.make('InvertedDoublePendulum-v2')))
             policy = GaussianMLPPolicy(
                 env_spec=env.spec,
                 hidden_sizes=(64, 64),
@@ -45,7 +45,7 @@ class TestTRPO(TfGraphTestCase):
 
     def test_trpo_unknown_kl_constraint(self):
         """Test TRPO with unkown KL constraints."""
-        env = TfEnv(normalize(gym.make("InvertedDoublePendulum-v2")))
+        env = TfEnv(normalize(gym.make('InvertedDoublePendulum-v2')))
         policy = GaussianMLPPolicy(
             env_spec=env.spec,
             hidden_sizes=(64, 64),
@@ -68,8 +68,37 @@ class TestTRPO(TfGraphTestCase):
                 gae_lambda=0.98,
                 policy_ent_coeff=0.0,
                 plot=False,
-                kl_constraint="random kl_constraint",
+                kl_constraint='random kl_constraint',
             )
-        assert "Unknown KLConstraint" in str(context.exception)
+        assert 'Unknown KLConstraint' in str(context.exception)
 
         env.close()
+
+    def test_trpo_soft_kl_constraint(self):
+        """Test TRPO with unkown KL constraints."""
+        with LocalRunner(self.sess) as runner:
+            env = TfEnv(normalize(gym.make('InvertedDoublePendulum-v2')))
+            policy = GaussianMLPPolicy(
+                env_spec=env.spec,
+                hidden_sizes=(64, 64),
+                hidden_nonlinearity=tf.nn.tanh,
+                output_nonlinearity=None,
+            )
+            baseline = GaussianMLPBaseline(
+                env_spec=env.spec,
+                regressor_args=dict(hidden_sizes=(32, 32)),
+            )
+            algo = TRPO(
+                env_spec=env.spec,
+                policy=policy,
+                baseline=baseline,
+                max_path_length=100,
+                discount=0.99,
+                gae_lambda=0.98,
+                policy_ent_coeff=0.0,
+                kl_constraint='soft')
+            runner.setup(algo, env)
+            last_avg_ret = runner.train(n_epochs=10, batch_size=2048)
+            assert last_avg_ret > 50
+
+            env.close()
